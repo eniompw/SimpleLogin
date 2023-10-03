11b07f622ad48273675b650c2b98560bc90cf3b4
eniompw <71254710+eniompw@users.noreply.github.com>
2023-10-03 17:04:48 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+	return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+	return render_template('signup.html')
+
+@app.route('/select', methods=['post'])
+def select():
+    con = sqlite3.connect('login.db')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE username=? AND password=?",
+                    (request.form['un'],request.form['pw']))
+    match = len(cur.fetchall())
+    con.close()
+    if match == 0:
+        return "wrong username and password"
+    else:
+        return "welcome " + request.form['un']
+
+@app.route('/insert', methods=['post'])
+def insert():
+	con = sqlite3.connect("login.db")
+	cur = con.cursor()
+	cur.execute(""" INSERT INTO users (username, password)
+					VALUES (?, ?) """,
+					(request.form['un'], request.form['pw'])	)
+	con.commit()
+	con.close()
+	return 'signup successful'
